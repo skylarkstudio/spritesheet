d062c80ebadf26611f751f73c7c7a7cc818a443c
Mario G. Pozzo <mpozzo@kinexo.com>
2012-01-18 10:23:48 -0300

    Changed Zoe Importer to parse latest file format.

--- com/eclecticdesignstudio/spritesheet/importers/Zoe.hx
+++ com/eclecticdesignstudio/spritesheet/importers/Zoe.hx
@@ -30,29 +30,48 @@ class Zoe {
 	public static function parse (data:String, assetDirectory:String = "", spriteSheetName:String = ""):SpriteSheet {		
 		var json:Dynamic = JSON.parse(data);
 		
-		// Create an array of frames based on the table of images created in the zoe exported png
-		var image = new Bitmap(Assets.getBitmapData(assetDirectory + "/" + json.src));
-		var columnCount:Int = Math.floor(image.width / json.w);
-		var rowCount:Int = Math.floor(image.height / json.h);
+		var images:Array<Bitmap> = [];
+		
+		for (i in 0...json.images.length) {
+			images.push(new Bitmap(Assets.getBitmapData(assetDirectory + "/" + json.images[i])));
+		}
+		
 		var frames:Array<SpriteSheetFrame> = new Array <SpriteSheetFrame>();
 		
-		for (row in 0...rowCount) {
-			for (column in 0...columnCount) {
-				frames.push(new SpriteSheetFrame(Math.floor(column * json.w), Math.floor(row * json.h), json.w, json.h, 0, 0));
+		if (Std.is(json.frames, Array) ) {
+			for (i in 0...json.frames.length) {
+				// json.frames[i][4] contains the image in multi image files
+				frames.push(new SpriteSheetFrame(json.frames[i][0], json.frames[i][1], json.frames[i][2], json.frames[i][3], -json.frames[i][5], -json.frames[i][6]));
+			}
+		} else {
+			var columns:Int = Math.floor(images[0].width / json.frames.width);
+			var rows:Int = Math.floor(images[0].height / json.frames.height);
+			
+			for (k in 0...rows) {
+				for (i in 0...columns) {
+					frames.push(new SpriteSheetFrame(Std.int(i * json.frames.width), Std.int(k * json.frames.height), json.frames.width, json.frames.height, 0, 0));
+				}
 			}
 		}
 		
 		// Populate the array of behaviors from the frames specified in the states list in the json file
 		var behaviors:Hash<BehaviorData> = new Hash<BehaviorData>();
-		for (key in Reflect.fields(json.states)) {
-			var state = Reflect.field(json.states, key);
+		for (key in Reflect.fields(json.animations)) {
+			var state = Reflect.field(json.animations, key);
 			var behaviorFrames:Array<Int> = new Array<Int>();
-			
-			for (i in state.start...state.end +1) {
-				behaviorFrames.push(i);
+			var frames = Reflect.field(state, "frames");
+						
+			if (frames == null) {
+				//there must be a more elegant way of doing this...
+				var list_frames:Array<Int> = Reflect.field(json.animations, key);
+				for (i in list_frames[0]...list_frames[1]) {
+					behaviorFrames.push(i);
+				}
+			} else {
+				behaviorFrames = Reflect.field(state, "frames");
 			}
 			
-			var origin = new Point(frames[state.start].x, frames[state.start].y);
+			//var origin = new Point(frames[state.start].x, frames[state.start].y);
 			//var behavior = new BehaviorData(key, behaviorFrames, true, 30, origin.x, origin.y);
 			var behavior = new BehaviorData(key, behaviorFrames, true, 30, 0, 0);
 			behaviors.set(behavior.name, behavior);
@@ -60,14 +79,14 @@ class Zoe {
 		
 		// Create the spriteSheet object
 		if (spriteSheetName == "") {
-			var spriteSheetName = json.src.split(".")[0];
+			var spriteSheetName = json.images[0].split(".")[0];
 			if (spriteSheetName == "") {
 				spriteSheetName = "undefined";
 			}
 		}
 		var spriteSheet:SpriteSheet = new SpriteSheet (frames, behaviors);
 		spriteSheet.name = spriteSheetName;
-		spriteSheet.setImage(Assets.getBitmapData(assetDirectory + "/" + json.src));
+		spriteSheet.setImage(Assets.getBitmapData(assetDirectory + "/" + json.images[0]));
 		
 		return spriteSheet;
 	}
