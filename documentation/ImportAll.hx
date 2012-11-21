6008c16967ef6ae44f868184e968c97b9b4dba4c
Joshua Granick <joshua@joshuagranick.com>
2012-11-20 16:02:28 -0800

    Updated documentation

--- documentation/ImportAll.hx
+++ documentation/ImportAll.hx
@@ -1,8 +1,9 @@
 package;
 
 import com.eclecticdesignstudio.spritesheet.data.BehaviorData;
-import com.eclecticdesignstudio.spritesheet.data.SpriteSheetFrame;
-import com.eclecticdesignstudio.spritesheet.importers.SpriteLoq;
-import com.eclecticdesignstudio.spritesheet.importers.Zoe;
+import com.eclecticdesignstudio.spritesheet.data.SpritesheetFrame;
+import com.eclecticdesignstudio.spritesheet.importers.AnimoImporter;
+import com.eclecticdesignstudio.spritesheet.importers.BitmapImporter;
+import com.eclecticdesignstudio.spritesheet.importers.ZoeImporter;
 import com.eclecticdesignstudio.spritesheet.AnimatedSprite;
-import com.eclecticdesignstudio.spritesheet.SpriteSheet;
+import com.eclecticdesignstudio.spritesheet.Spritesheet;
